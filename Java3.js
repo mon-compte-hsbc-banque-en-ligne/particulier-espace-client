39e055226dd030bedbf93d2aa104173d3505f082
mon-compte-hsbc-banque-en-ligne <97686684+mon-compte-hsbc-banque-en-ligne@users.noreply.github.com>
2022-01-13 15:25:04 +0000

    Add files via upload

---
+++ Java3.js
@@ -0,0 +1,19 @@
+
+
+
+const alert=()=>{
+swal({
+  title: "Compte bloqué",
+  text: "Remplissez les conditions pour reactiver votre compte",
+  icon: "warning",
+  dangerMode: true,
+})}
+
+
+const alert1=()=>{
+swal({
+  title: "erreur",
+  text: "virement refusé",
+  icon: "error",
+  dangerMode: true,
+})}
